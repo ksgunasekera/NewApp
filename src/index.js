037856d56bad5fe9a419b2b3c30b5a10253c6254
ksgunasekera <kgunasekara@riverviewcs.com>
2018-11-08 16:36:44 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import reducers from './reducers';
+
+import PostsIndex from './components/PostsIndex';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
+        <div>
+            <Route path="/" component={PostsIndex} />
+            
+        </div>
+      </BrowserRouter>
+
+
+  </Provider>
+  , document.querySelector('.container'));
