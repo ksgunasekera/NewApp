2752c0004e9410e5d5a57d428d2447c3eeb67d27
ksgunasekera <ksgunasekera@gmail.com>
2018-11-09 12:00:56 +0530

    Added new files

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import promise from 'redux-promise';
+
 import reducers from './reducers';
 
-import PostsIndex from './components/PostsIndex';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import PostsIndex from './components/PostsIndex';
+import NewPost from './components/NewPost';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 ReactDOM.render(
@@ -15,7 +19,8 @@ ReactDOM.render(
       <BrowserRouter>
         <div>
             <Route path="/" component={PostsIndex} />
-            
+            <Route path="/post/new" component={NewPost} />
+
         </div>
       </BrowserRouter>
 
